9500ccae849e1ed94bf7b4d3217de86a0d77bf06
Ozodchik <dilovardehkanov730@gmail.com>
2020-10-19 12:03:40 +0300

    commited last

--- second_format.py
+++ second_format.py
@@ -1,23 +1,24 @@
 import xml.etree.ElementTree as ET
+
 parser = ET.XMLParser(encoding="utf-8")
 tree = ET.parse("newsafr.xml", parser)
 root = tree.getroot()
 from collections import Counter
 
-def get(doc):
-  results = []
-  open_file = root
-  find_file = open_file.findall("channel/item")
-  for result in find_file:
-    first_result = result.find("description").text.lower().split()
-    for second_result in first_result:
-      if len(second_result) > 6:
-        results.append(second_result)
 
-  last_list = Counter(results)
-  sorted_list = last_list.most_common(10)
-  return f'топ 10 самые часто используемые слова:{sorted_list}'
+def get(filename: str, top_size: int, min_word_len: int) -> str:
+    all_words = []
+    open_file = root
+    find_file = open_file.findall("channel/item")
+    for article in find_file:
+        words = article.find("description").text.lower().split()
+        for word in words:
+            if len(word) > min_word_len:
+                all_words.append(word)
+    counter = Counter(all_words)
+    sorted_list = counter.most_common(10)
+    return f"топ {top_size} самые часто используемые слова:{sorted_list}"
 
-print(get("newsafr.xml"))
+print(get("newsafr.xml", 10, 6))
 
   
